d523e2384452f9164ea7edf346b5f888d6e8e4a5
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-10-31 18:40:42 +0900

    fix Timer

--- src/Signal/Time.purs
+++ src/Signal/Time.purs
@@ -1,22 +1,25 @@
-module Signal.Time where
+module Signal.Time
+  ( every
+  ) where
 
 import Prelude
 
 import Data.DateTime.Instant (unInstant)
 import Data.Int (floor)
-import Data.Time.Duration (Milliseconds(..))
+import Data.Newtype (unwrap)
 import Effect (Effect)
 import Effect.Now (now)
 import Effect.Timer (setInterval)
 import Signal (Signal, newChannel, send, subscribe)
 
+nowMilliseconds :: Effect Number
+nowMilliseconds = unwrap <<< unInstant <$> now
+
 every :: Int -> Effect (Signal Int)
 every dt = do
   chn <- newChannel
-  Milliseconds st <- unInstant <$> now
+  st <- nowMilliseconds
   _ <- setInterval dt do
-    Milliseconds nt <- unInstant <$> now
+    nt <- nowMilliseconds
     send chn $ floor $ nt - st
-  pure $ subscribe chn do
-    Milliseconds nt <- unInstant <$> now
-    pure $ floor $ nt - st
+  pure $ subscribe chn $ floor <$> nowMilliseconds
